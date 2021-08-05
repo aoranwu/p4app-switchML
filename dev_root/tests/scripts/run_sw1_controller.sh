27e76e29786372ce2bf2ad3a46ec0153e2cf31bd
aoranwu <awu48@wisc.edu>
2021-08-05 11:55:43 -0700

    before integrating controllers

--- dev_root/tests/scripts/run_sw1_controller.sh
+++ dev_root/tests/scripts/run_sw1_controller.sh
@@ -1,2 +1,2 @@
 cd /home/aoranwu/p4app-switchML/dev_root/controller/
-ip netns exec sw1 python switchml.py --switch-mac=06:00:00:00:00:01 --switch-ip=198.19.200.201 --ports=/home/aoranwu/p4app-switchML/dev_root/controller/ports1.yaml --use-model --switch-name=sw1 --switch-conf-file=/home/aoranwu/p4app-switchML/dev_root/controller/switch_conf.yaml
+ip netns exec sw1 python switchml.py --switch-mac=06:00:00:00:00:01 --switch-ip=198.19.200.201 --ports=/home/aoranwu/p4app-switchML/dev_root/controller/ports1.yaml --use-model --switch-name=sw1 --switch-conf-file=/home/aoranwu/p4app-switchML/dev_root/controller/switch_conf.yaml --bfrt-ip=192.168.1.2 --bfrt-port=50052
